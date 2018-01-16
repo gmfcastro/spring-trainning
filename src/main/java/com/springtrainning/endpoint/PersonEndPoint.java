8888b1a1158162c63c95345fb79b14432c90a53e
jvcaquino <jvcaquino95@gmail.com>
2018-01-15 23:23:16 -0200

    Spring Data JPA with MYSQL

---
+++ src/main/java/com/springtrainning/endpoint/PersonEndPoint.java
@@ -0,0 +1,57 @@
+package com.springtrainning.endpoint;
+
+
+import com.springtrainning.error.CustomErrorType;
+import com.springtrainning.model.Person;
+import com.springtrainning.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/people")
+public class PersonEndPoint {
+
+    private final PersonRepository personDAO;
+
+    @Autowired
+    public PersonEndPoint(PersonRepository personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listAll(){
+        return new ResponseEntity<>(personDAO.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getPersonById(@PathVariable("id") Long id){
+        Person person = personDAO.findOne(id);
+        if (person == null)
+            return new ResponseEntity<>(new CustomErrorType("Person not found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{name}")
+    public ResponseEntity<?> findPersonByName(@PathVariable String name){
+        return new ResponseEntity<>(personDAO.findByName(name), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Person person){
+        return new ResponseEntity<>(personDAO.save(person), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        personDAO.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Person person){
+        personDAO.save(person);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/springtrainning/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.springtrainning.repository;
+
+import com.springtrainning.model.Person;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PersonRepository extends CrudRepository<Person, Long>{
+    List<Person> findByName(String name);
+}
