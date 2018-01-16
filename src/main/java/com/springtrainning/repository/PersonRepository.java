ccb986b01da7c054144e2be48214116bd3f7d9e5
jvcaquino <jvcaquino95@gmail.com>
2018-01-16 10:10:48 -0200

    Spring Data JPA with MongoDB v3

--- src/main/java/com/springtrainning/repository/PersonRepository.java
+++ src/main/java/com/springtrainning/repository/PersonRepository.java
@@ -2,9 +2,11 @@ package com.springtrainning.repository;
 
 import com.springtrainning.model.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PersonRepository extends CrudRepository<Person, Long>{
     List<Person> findByNameIgnoreCaseContaining(String name);
 }
